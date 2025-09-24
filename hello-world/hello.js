559db38df26d9ad61df7dbada5c98f60b3328175
Sanoj <panditsanoj124@gmail.com>
2025-09-24 12:10:38 +0530

    understanding basics of npm, node and js difference, package.json file etc

---
+++ hello-world/hello.js
@@ -0,0 +1,15 @@
+// you can run this in terminal using node hello.js
+console.log("hey there i am js"); 
+
+// you can run this in browser console as well
+
+// this will give error in terminal
+// console.log(window); 
+// problem is that DOM or Window related elements are not available in node environment
+
+
+// we need to do npm init -y
+// it will create the package.json file
+// package.json file contains metadata about the project
+// we can change the name and version in package.json file and add scripts like we create start script
+
