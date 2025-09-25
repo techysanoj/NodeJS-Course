081ea9dedcb06146b67899b3dba811898884f144
Sanoj <panditsanoj124@gmail.com>
2025-09-25 12:23:33 +0530

    understanding basic of express js

--- server/index.js
+++ server/index.js
@@ -1,57 +1,93 @@
-// main file should be named with index.js
-// because it is a main file which a programmer starts looking for
+// // main file should be named with index.js
+// // because it is a main file which a programmer starts looking for
+
+// const http = require("http"); // importing built in http module
+
+// const fs = require("fs");
+
+// const url = require("url"); // importing downloaded url module
+
+// const myServer = http.createServer((req, res) => {
+//     // console.log('New Request recieved: ');
+//     // console.log("Request: ", req);
+//     if(req.url === '/favicon.ico') {
+//         res.end(); // to end the response
+//         return; // to exit the function
+//     }
+//     // const log = `${Date.now()}: - ${req.url} New request received \n`;
+//     const log = `${Date.now()}: - ${req.url} Method is - ${req.method} New request received \n`; 
+//     // by default it is get request
+
+//     const myUrl = url.parse(req.url, true); // true to get complete url object
+
+//     console.log(myUrl);
+
+//     fs.appendFile("log.txt", log, (err, data) => {
+//         // res.end("Hello from server \n Logged successfully");
+//         switch(myUrl.pathname) { // changed accordingly
+//             case '/': 
+//                 if(req.method === 'GET') {
+//                     res.end("GET Request on Home page");
+//                 }
+//                 break
+//             case '/about': 
+//                 // console.log(myUrl.query); // to get the query parameters
+//                 const name = myUrl.query.myname || "Guest"; // to get the name from query parameters
+//                 res.end(`Hello ${name}, Welcome to About us page`);
+//                 break
+//             case '/signup':
+//                     if(req.method === 'GET') {
+//                         res.end("This is a sign up form");
+//                     }
+//                     else if(req.method === 'POST') {
+//                         res.end("Form Submitted successfully");
+//                     }
+//                 break
+//             default: res.end("404 Page Not Found");
+//         }
+
+//     })
+// }); // creating a server and it takes a callback function reqest listener
+
+// // to run this server we will use listen method on some port
+
+// myServer.listen(8000, () => console.log("Server is running on port 8000")); // listen method takes a port number and a callback function which will be called when the server starts listening on the port
+
+// // if we have changed anything then you need to restart the server again using ctrl + c to exit the server and then run the server
+
+// // always try to do non blocking task because it may block the threadpool
+
+
+// // why do we need express js?
+// // because for every route we need to write a lot of code
+// // and then for every request we need to check the method and then the route
+// // so to make it easy we use express js
+
+
+const express = require("express"); // importing express module
 
 const http = require("http"); // importing built in http module
 
-const fs = require("fs");
+const app = express(); // creating an express app
+// app is basically that handler function
 
-const url = require("url"); // importing downloaded url module
+app.get("/", (req, res) => {
+    return res.send("Hello from home page");
+})
 
-const myServer = http.createServer((req, res) => {
-    // console.log('New Request recieved: ');
-    // console.log("Request: ", req);
-    if(req.url === '/favicon.ico') {
-        res.end(); // to end the response
-        return; // to exit the function
-    }
-    // const log = `${Date.now()}: - ${req.url} New request received \n`;
-    const log = `${Date.now()}: - ${req.url} Method is - ${req.method} New request received \n`; 
-    // by default it is get request
+app.get("/about", (req, res) => {
+    return res.send("Hello from about page" + ' ' + (req.query.name) + ' ' + (req.query.age));
+})
 
-    const myUrl = url.parse(req.url, true); // true to get complete url object
+app.post("/signup", (req, res) => {
+    return res.send("Form submitted successfully");
+})
 
-    console.log(myUrl);
+// const myServer = http.createServer(app);
 
-    fs.appendFile("log.txt", log, (err, data) => {
-        // res.end("Hello from server \n Logged successfully");
-        switch(myUrl.pathname) { // changed accordingly
-            case '/': 
-                if(req.method === 'GET') {
-                    res.end("GET Request on Home page");
-                }
-                break
-            case '/about': 
-                // console.log(myUrl.query); // to get the query parameters
-                const name = myUrl.query.myname || "Guest"; // to get the name from query parameters
-                res.end(`Hello ${name}, Welcome to About us page`);
-                break
-            case '/signup':
-                    if(req.method === 'GET') {
-                        res.end("This is a sign up form");
-                    }
-                    else if(req.method === 'POST') {
-                        res.end("Form Submitted successfully");
-                    }
-            default: res.end("404 Page Not Found");
-        }
+// myServer.listen(8000, () => console.log("Server is running on port 8000")); 
 
-    })
-}); // creating a server and it takes a callback function reqest listener
+app.listen(8000, () => console.log("Server is running on port 8000")); // now using this we can run the server directly without creating http server explicitly
 
-// to run this server we will use listen method on some port
-
-myServer.listen(8000, () => console.log("Server is running on port 8000")); // listen method takes a port number and a callback function which will be called when the server starts listening on the port
-
-// if we have changed anything then you need to restart the server again using ctrl + c to exit the server and then run the server
-
-// always try to do non blocking task because it may block the threadpool
+// basic routing
+// app.method(path, handler_function)
